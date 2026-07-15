3cc0477694b51d4a83af2ada6c279d97ecb59cb4
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-07-15 13:39:07 +0300

    feat : all services for projects

---
+++ src/services/project.service.ts
@@ -0,0 +1,46 @@
+import prisma from "../prismaClient";
+import { findProjectOrThrow } from "../utils/project";
+
+// get all projects
+export async function getAllProjects(userId: number) {
+  return prisma.project.findMany({
+    where: { userId },
+    orderBy: { createdAt: "desc" },
+  });
+}
+
+// get specifics project
+export async function getProjectById(userId: number, id: number) {
+  return prisma.project.findFirst({
+    where: { id, userId },
+    include: { todos: true },
+  });
+}
+
+// project creation
+export async function createProject(data: {
+  name: string;
+  description?: string;
+  userId: number;
+}) {
+  return prisma.project.create({ data });
+}
+
+// update project
+export async function updateProject(
+  userId: number,
+  id: number,
+  data: {
+    name?: string;
+    description?: string;
+  },
+) {
+  await findProjectOrThrow(userId, id);
+  return prisma.project.update({ where: { id }, data });
+}
+
+// delete project
+export async function deleteProject(userId: number, id: number) {
+  await findProjectOrThrow(userId, id);
+  return prisma.project.delete({ where: { id } });
+}
